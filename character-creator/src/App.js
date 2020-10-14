da2fc86ca9ade30bd8492ba44309126c00c44f78
github username <github email address>
2020-10-14 19:07:47 -0400

    wrote function for api key

--- character-creator/src/App.js
+++ character-creator/src/App.js
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios'
 import './App.css';
 
 function App() {
+  const [image, setImage] = useState('')
+
+  useEffect(() => { 
+    axios.get('https://api.generated.photos/api/v1/faces?api_key=-oSJ9MnkYiiimwgLEOefKQ').then(res => {
+      console.log(res.data)
+  }).catch(err => {
+    console.log(err.message)
+  })
+})
+    
   return (
     <div className="App">
     <h1> character creator </h1>
